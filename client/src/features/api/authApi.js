afedb9c94c18f0c1717f57879c5f35c32f37a771
Ritesh Singh <yesfirst000@gmail.com>
2025-09-06 00:47:47 +0530

    authApi

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const USER_API = "https://e-learning-dhr4.onrender.com" || "http://localhost:5000";
+const USER_API = "https://e-learning-dhr4.onrender.com/api/v1/user" || "http://localhost:5000/api/v1/user";
 
 export const authApi = createApi({
     reducerPath:"authApi",
